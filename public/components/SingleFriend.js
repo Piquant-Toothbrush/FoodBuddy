86e10b102e353972bc1a114717a3f82fe52c0b79
Krishan Arya <krishanmarya1@gmail.com>
2016-09-27 23:59:59 +1000

    Fix bug related to friendName

--- public/components/SingleFriend.js
+++ public/components/SingleFriend.js
@@ -1,13 +1,13 @@
 var SingleFriend = ({moviesOfFriend,onClick,friendName, change}) => {
 
-
+console.log(friendName,'friendName');
 	// console.log('props.moviesOfFriend', moviesOfFriend)
 	if (!moviesOfFriend.length){
 		return (
 			<div>
 			<a id="backToAllFriends" className="center waves-effect waves-light btn" onClick={() => (onClick("Friends"))}>Back to all friends</a>
 			
-			<h5 id="noFriendMovies" class="header lable">Sorry, {friendName.slice(friendName.indexOf(">") + 1, friendName.lastIndexOf("<")+1)} hasn't rated any movies.</h5>
+			<h5 id="noFriendMovies" class="header lable">Sorry, {friendName} hasn't rated any movies.</h5>
 			</div>
 			)
 
